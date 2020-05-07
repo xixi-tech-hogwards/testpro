9b92348253a8f7a6ed6d3a1b7243b1faf8f6dab7
xixi-tech-hogwards <xixi@testerhome.com>
2020-05-07 15:59:09 +0800

    第一次提交python代码

---
+++ testing/demo2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# print("demo2")
+hello = 'hello python'
+
+
+def f2():
+    print(" f2")
+
+
+class Demo2:
+    def add(self):
+        print(" add")
